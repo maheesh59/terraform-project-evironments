f1966116f5f28aafaf1a4205b87efc1a8b85d159
root <root@ip-172-31-31-167.ap-south-1.compute.internal>
2026-08-24 11:19:42 +0000

    Finalize test environment configuration with dynamic KMS lookup

--- environments/test/backend.tf
+++ environments/test/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "global-tfstate-010160406667"
+    bucket       = "global-tfstate-463200133372"
     key          = "environments/test/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
